e2fd5f3906269131833402cdbb2eb60d2a28b18e
dnjsp <whdhsp1@naver.com>
2022-06-29 14:59:42 +0900

    배포하기

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -1,22 +1,20 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const ProductCard = ({ item }) => {
-
   const navigate = useNavigate();
-  const showDetail = () => {
-    navigate(`/product/${item.id}`)
-  }
-
+  const showProduct = (id) => {
+    navigate(`/product/${id}`);
+  };
   return (
-    <div className='card' onClick={showDetail}>
+    <div className="card" onClick={() => showProduct(item.id)}>
       <img src={item?.img} />
-      <div>{item?.choice == true ? "Concious choice" : ""}</div>
+      <div className="choice">{item?.choice ? "Conscious choice" : ""}</div>
       <div>{item?.title}</div>
-      <div>{item?.price}</div>
-      <div>{item?.new == true ? "신제품" : ""}</div>
+      <div>₩{item?.price}</div>
+      <div className="new-product">{item?.new ? "신제품" : ""}</div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
--- src/page/Login.js
+++ src/page/Login.js
@@ -1,40 +1,32 @@
-import React from 'react'
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
-
+import React from "react";
+import { Container, Form, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 const Login = ({ setAuthenticate }) => {
   const navigate = useNavigate();
-
-  const loginUser = (event) => {
+  const login = (event) => {
     event.preventDefault();
     setAuthenticate(true);
-    navigate('/')
-  }
-
+    navigate("/");
+  };
   return (
-    <Container>
-      <Form onSubmit={(event) => loginUser(event)}>
+    <Container className="login-area">
+      <Form className="login-form" onSubmit={login}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
+
         <Button variant="danger" type="submit">
-          로그인
+          Login
         </Button>
       </Form>
     </Container>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
