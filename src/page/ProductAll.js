3069422a9e2bc3f0a9cbf2a352a00e432f5968f1
dnjsp <whdhsp1@naver.com>
2022-06-29 10:16:37 +0900

    API 부르기

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../component/ProductCard';
 
 const ProductAll = () => {
+
+  const [productList, setProductList] = useState([]);
+
+  const getProducts = async () => {
+    let url = `http://localhost:5000/products`
+    let response = await fetch(url)
+    let data = await response.json();
+    setProductList(data);
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, [])
+
   return (
     <div>
-      전체 상품 페이지
+      <ProductCard />
     </div>
   )
 }
